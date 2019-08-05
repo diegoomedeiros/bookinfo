06d586ffb345dabcf3e51590ae1529edcb9f7c6c
Diego <diegoomedeiros@gmail.com>
2019-08-05 19:29:00 -0300

    Alteracao de versao do Microsk8 (Bug de inicializacao de Pod)

--- escopobasico.sh
+++ escopobasico.sh
@@ -1,24 +1,36 @@
-echo "Verificando instalação do Microk8s"
+set -e
+LIGHT_BLUE='\033[1;34m'
+NC='\033[0m'
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Verificando instalação do Microk8s${NC}"
+echo ""
 PROGRAMA=$(type microk8s | grep microk8s | wc -l)
 if [ $PROGRAMA -eq 0 ]
 then
-echo "Instalando microk8s"
-snap install microk8s --classic
+echo -e "${LIGHT_BLUE}Instalando microk8s${NC}"
+snap install microk8s --classic --channel=1.16/edge
 else
-echo "Microk8s já está instalado"
+echo -e "${LIGHT_BLUE}Microk8s já está instalado${NC}"
 fi
 
-echo "Configurando Alias para Kubectl"
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Configurando Alias para Kubectl${NC}"
+echo ""
 sudo snap alias microk8s.kubectl kubectl
 sleep 5
-
-echo "Configurando permissoes"
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Configurando permissoes${NC}"
+echo ""
 echo "--allow-privileged=true" | sudo tee -a /var/snap/microk8s/current/args/kubelet
 echo "--allow-privileged=true" | sudo tee -a /var/snap/microk8s/current/args/kube-apiserver
-
-
+echo ""
+echo ""
 sleep 4
-echo "Adicionando Regras de Firewall" ##Debian/Ubuntu
+echo -e "${LIGHT_BLUE}Adicionando Regras de Firewall${NC}" ##Debian/Ubuntu
+echo ""
 sudo ufw default allow routed
 sudo iptables -P FORWARD ACCEPT
 
@@ -26,19 +38,29 @@ sudo iptables -P FORWARD ACCEPT
 #sudo microk8s.enable dns dashboard metrics-server  
 #sudo ufw allow in on cbr0 && sudo ufw allow out on cbr0 # Dns e Dashboar com CrashLopping
 #sleep 5
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Habilitando Istio${NC}"
+echo ""
+microk8s.enable istio 
+sleep 5
 
-echo "Habilitando Istio"
-sudo microk8s.enable istio 
-sleep 10
-
-#echo "Criando namespace Bookinfo"
+#echo "${LIGHT_BLUE}Criando namespace Bookinfo${NC}"
 #kubectl create namespace bookinfo-app
-
-echo "Habilitando o istio-injection no namespace"
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Habilitando o istio-injection no namespace${NC}"
+echo ""
 kubectl label namespace default istio-injection=enabled
-sleep 10
-echo "Implantando aplicação"
-kubectl create -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/platform/kube/bookinfo.yaml
-kubectl create -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/networking/bookinfo-gateway.yaml
+sleep 5
+echo ""
+echo ""
+echo -e "${LIGHT_BLUE}Implantando aplicação${NC}"
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/networking/bookinfo-gateway.yaml
 #kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/networking/destination-rule-all-mtls.yaml
+
+
+
+
 

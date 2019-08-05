6eceecebfb990e4e4cec418ba25d1f6ce821c3cd
Diego <diegoomedeiros@gmail.com>
2019-08-05 20:48:06 -0300

    Mais estilizacao e config de interface CBR em firewall

--- escopobasico.sh
+++ escopobasico.sh
@@ -5,13 +5,14 @@ echo ""
 echo ""
 echo -e "${LIGHT_BLUE}Verificando instalação do Microk8s${NC}"
 echo ""
-PROGRAMA=$(type microk8s | grep microk8s | wc -l)
+PROGRAMA=$(type microk8s 2>/dev/null | grep microk8s | wc -l)
 if [ $PROGRAMA -eq 0 ]
-then
-echo -e "${LIGHT_BLUE}Instalando microk8s${NC}"
-snap install microk8s --classic --channel=1.16/edge
-else
-echo -e "${LIGHT_BLUE}Microk8s já está instalado${NC}"
+	then
+		
+		echo -e "${LIGHT_BLUE}Instalando microk8s${NC}"
+		snap install microk8s --classic --channel=1.14/stable
+	else
+	echo -e "${LIGHT_BLUE}Microk8s já está instalado${NC}"
 fi
 
 echo ""
@@ -19,13 +20,13 @@ echo ""
 echo -e "${LIGHT_BLUE}Configurando Alias para Kubectl${NC}"
 echo ""
 sudo snap alias microk8s.kubectl kubectl
+echo ""
 sleep 5
 echo ""
+echo -e "${LIGHT_BLUE}Configurando permissoes de execucao (Eternal Pending Status)${NC}"
 echo ""
-echo -e "${LIGHT_BLUE}Configurando permissoes${NC}"
-echo ""
-echo "--allow-privileged=true" | sudo tee -a /var/snap/microk8s/current/args/kubelet
-echo "--allow-privileged=true" | sudo tee -a /var/snap/microk8s/current/args/kube-apiserver
+echo "--allow-privileged=true" 2>/dev/null | sudo tee -a /var/snap/microk8s/current/args/kubelet
+echo "--allow-privileged=true" 2>/dev/null | sudo tee -a /var/snap/microk8s/current/args/kube-apiserver
 echo ""
 echo ""
 sleep 4
@@ -33,7 +34,8 @@ echo -e "${LIGHT_BLUE}Adicionando Regras de Firewall${NC}" ##Debian/Ubuntu
 echo ""
 sudo ufw default allow routed
 sudo iptables -P FORWARD ACCEPT
-
+sudo ufw allow in on cbr0 && sudo ufw allow out on cbr0
+sleep 2
 #echo "Habilitando Dashboard, DNS e Metrics-Server" 
 #sudo microk8s.enable dns dashboard metrics-server  
 #sudo ufw allow in on cbr0 && sudo ufw allow out on cbr0 # Dns e Dashboar com CrashLopping
@@ -43,7 +45,7 @@ echo ""
 echo -e "${LIGHT_BLUE}Habilitando Istio${NC}"
 echo ""
 microk8s.enable istio 
-sleep 5
+sleep 10
 
 #echo "${LIGHT_BLUE}Criando namespace Bookinfo${NC}"
 #kubectl create namespace bookinfo-app

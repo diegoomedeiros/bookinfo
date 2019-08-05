39a887bcf7e3bac12e97778ea45acb6b48489bd4
Diego <diegoomedeiros@gmail.com>
2019-08-05 10:39:12 -0300

    Adicionado trecho de Implatação do Bookinfo

--- escopobasico.sh
+++ escopobasico.sh
@@ -1,25 +1,36 @@
+echo "Verificando instalação do Microk8s"
+PROGRAMA=$(type microk8s | grep microk8s | wc -l)
+if [ $PROGRAMA -eq 0 ]
+then
 echo "Instalando microk8s"
 sudo snap install microk8s --classic
+else
+echo "Microk8s já está instalado"
+fi
+
 echo "Configurando Alias para Kubectl"
 sudo snap alias microk8s.kubectl kubectl
 
-echo "Habilitando Istio e DNS"
-sudo microk8s.enable istio  && sleep 5
+echo "Adicionando Regras de Firewall"
+sudo ufw default allow routed
+sudo iptables -P FORWARD ACCEPT
 
-echo "Habilitando Dashboard"
-sudo microk8s.enable dashboard
+echo "Habilitando Dashboard, DNS e Metrics-Server" 
+sudo microk8s.enable dns dashboard metrics-server  
+sudo ufw allow in on cbr0 && sudo ufw allow out on cbr0 # Dns e Dashboar com CrashLopping
+sleep 5
 
-echo "Configurando aliases"
-sudo snap alias microk8s.kubectl kubectl
-sudo snap alias microk8s.istioctl istioctl
+echo "Habilitando Istio"
+sudo microk8s.enable istio 
 
-
-echo "Criando namespace Bookinfo"
-kubectl create namespace bookinfo
+#echo "Criando namespace Bookinfo"
+#kubectl create namespace bookinfo-app
 
 echo "Habilitando o istio-injection no namespace"
-kubectl label namespace bookinfo istio-injection=enabled
+kubectl label namespace default istio-injection=enabled
 
 echo "Implantando aplicação"
-kubectl apply -f 
+kubectl create -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/platform/kube/bookinfo.yaml
+kubectl create -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/networking/bookinfo-gateway.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.2/samples/bookinfo/networking/destination-rule-all-mtls.yaml
 
